02781a9075d6ff9d68d1d61ec8ad6e58f110eee0
Dan Mee <danmee10@gmail.com>
2013-03-25 16:54:35 -0600

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+
   validates_confirmation_of :password, message: "should match confirmation", if: :password
+  validates :password, :length => { :in => 8..16 }
+
+  validates :username, :presence => true, :uniqueness => true,
+                       :length => { :maximum => 16 }
+
+  validates :first_name, :presence => true,
+                         :format => { :with => /\A[a-zA-Z]+\z/,
+                                      :message => "Only letters allowed" }
+  validates :last_name, :presence => true,
+                        :format => { :with => /\A[a-zA-Z]+\z/,
+                                     :message => "Only letters allowed" }
+  validates :email, :presence => true,
+                    :format => { :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+                                 :message => "Please enter a valid email address"}
 end
