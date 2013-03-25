a50e4be664b51a05917ad544e672a20e4a9e9669
Dan Mee <danmee10@gmail.com>
2013-03-25 17:35:07 -0600

    deleted commented out shit in routes.rb, minor adjustments to format of user/products models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password, message: "should match confirmation", if: :password
   validates :password, :length => { :in => 8..16 }
 
-  validates :username, :presence => true, :uniqueness => true,
-                       :length => { :maximum => 16 }
+  validates :username, :uniqueness => { case_sensitive: false },
+                       :length => { :in => 2..32 }
 
   validates :first_name, :presence => true,
                          :format => { :with => /\A[a-zA-Z]+\z/,
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
                         :format => { :with => /\A[a-zA-Z]+\z/,
                                      :message => "Only letters allowed" }
   validates :email, :presence => true,
+                    :uniqueness => true,
                     :format => { :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
                                  :message => "Please enter a valid email address"}
 end
